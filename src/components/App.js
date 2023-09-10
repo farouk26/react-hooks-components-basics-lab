a11b7368d3c891c82e9543e5038b63769f2c7b71
Zemmouri Farouk <faroukwebdesign@gmail.com>
2023-09-10 16:00:04 +0100

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,11 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About (){
+  return (
+    <div id="about"><h2>About</h2></div>
+  )
+}
 
 
 function App() {
@@ -24,7 +29,10 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home/>
       {/* add your <About> component here */}
+      <About/>
+
     </div>
   );
 }
